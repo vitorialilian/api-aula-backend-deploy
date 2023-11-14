9a2afb51e30a59029024238ae8601c51ed757ab1
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2023-11-14 20:52:05 -0300

    configurações para api funcionar corretamente

---
+++ src/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+const express = require('express');
+const knex = require('./conexao');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    try {
+        const carros = await knex('carros')
+        return res.json(carros)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ mensagem: 'Erro no servidor.' })
+    }
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor em pé na porta ${port}`);
+})
