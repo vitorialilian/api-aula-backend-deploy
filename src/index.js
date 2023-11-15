f32784676e594df655e7579d0b7aa66be43a60db
Vitória Lilian <127898471+vitorialilian@users.noreply.github.com>
2023-11-15 14:54:22 -0300

    atualização app.listen

--- src/index.js
+++ src/index.js
@@ -22,5 +22,5 @@ app.get('/', async (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Servidor em pé na porta ${port}`);
+    console.log(`Servidor em pé na porta: ${port}`);
 })
